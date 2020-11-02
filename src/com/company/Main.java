3f7659df142bd7f89a38fbce4360fece277fd87f
DjouliCorn <tissier.julie@gmail.com>
2020-11-02 16:25:58 +0100

    guess a number

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,38 @@
 package com.company;
+import com.company.guessNumber;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int choice;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Choose your game :");
+        System.out.println("1 : Guess a number");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5 : Exit");
+
+        choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                guessNumber.playRandomNum();
+            case 2:
+                //jeu 2
+            case 3:
+                //jeu 3
+            case 4:
+                //jeu 4
+            case 5:
+                //quit
+            default:
+                System.out.println("Je n'ai pas compris votre demande");
+                Main.main(null);
+
+        }
     }
 }
---
+++ src/com/company/guessNumber.java
@@ -0,0 +1,43 @@
+package com.company;
+import com.company.Main;
+import java.util.Random;
+import java.util.Scanner;
+
+public class guessNumber {
+
+    public static void playRandomNum() {
+
+        Random rand = new Random();
+        int randInt = rand.nextInt(100);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(randInt);
+
+        int K = 10;
+
+        System.out.println("Guess a number in 10 trials :");
+
+       for (int i=0; i<=K; i++){
+
+           if (i == K){
+               System.out.println("You don't have anymore try.");
+               System.out.println("The number was "+randInt);
+           }
+
+           System.out.println("The number is between 0 and 100 :");
+           int userInput = scan.nextInt();
+
+           if (randInt == userInput){
+               System.out.println("You win !");
+               break;
+           }
+           else if(randInt<userInput && i != K-1){
+               System.out.println("Smaller");
+           }
+           else if (randInt>userInput && i != K-1){
+               System.out.println("Bigger");
+           }
+       }
+        Main.main(null);
+    }
+}
